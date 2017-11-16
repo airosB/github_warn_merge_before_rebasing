cb7afc0e676f71194fac4cce1a387b83eeddf1bb
airosB <airos.berlanathe@gmail.com>
2017-11-16 15:10:51 +0900

    押したあとで出るボタンとかにも適用されるように

--- greasemonkey_userscript.js
+++ greasemonkey_userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           gheでrebase前はマージボタンの見た目を変えて気づくようにするやつ
 // @namespace      https://github.com/airosB/github_warn_merge_before_rebasing
-// @version        1.00
+// @version        1.01
 // @description    ブランチをマージする前にrebaseでコミットを1つにしてないとマージボタンの色が変わる
 // @author         airosB
 // @match          https://github.com/*
@@ -15,14 +15,14 @@
     }
 
     var commitCount = $$('#commits_tab_counter')[0].innerText;
-    var mergeButton = $$('.btn-group-merge button')[0];
-    var mergeOptionButton = $$('.btn-group-merge button')[1];
+    var mergeButtons = $$('.btn-group-merge button');
+    var mergeButton = mergeButtons[0];
     mergeButton.innerText = mergeButton.innerText + ' (' + commitCount + ')';
 
     if(parseInt(commitCount, 10) >= 2){
-        mergeButton.classList.remove('btn-primary');
-        mergeButton.classList.add('btn-purple');
-        mergeOptionButton.classList.remove('btn-primary');
-        mergeOptionButton.classList.add('btn-purple');
+        mergeButtons.forEach(function(e){
+            e.classList.remove('btn-primary');
+            e.classList.add('btn-purple');
+        });
     }
 })();
